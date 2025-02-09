aa15c9572e371b47acafb1a762ca13e9cde42927
M-Aitisam <aitisamahmed24@gmail.com>
2025-02-09 11:59:09 +0500

    Working on  set cookie also  you  can use package to  wrk with  cookie-pasrser

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+// in  this no use of cookie oparser to  set cookie  in browser
+// app.get('/', (req, res) => {
+//    res.cookie("mycookiename", "mycookiedata");  // Server sa browser pa data set krana cookie ha
+//    // res.clearCookie("mycookiename"); // Server sa browser pa data delete kr
+//    res.send("Cookie Send");
+// });
 
 app.get('/', (req, res) => {
-    res.send('Welcome');
+   
+   res.send("Cookie Send");
 });
-
+app.get("/read", (req,res)=> {
+    res.send("Read Page");
+})
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
